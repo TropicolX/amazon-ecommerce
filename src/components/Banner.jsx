11abbe2f51122bbc5e647af08218c2ef4f71109e
TropicolX <jacobsbusayo@gmail.com>
2023-10-02 10:25:15 +0100

    banner updates

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { Carousel } from "react-responsive-carousel";
 
 import styles from "../styles/Banner.module.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -9,50 +8,23 @@ const Banner = () => {
 	const [imageLoaded, setImageLoaded] = useState(false);
 
 	useEffect(() => {
-		if (
-			document.querySelector(".slider.animated") &&
-			imageLoaded &&
-			!loaded
-		) {
+		if (imageLoaded && !loaded) {
 			setLoaded(true);
 		}
-	}, [document.querySelector(".slider.animated"), imageLoaded]);
+	}, [imageLoaded, loaded]);
 
 	return (
 		<div className={styles.container}>
 			{!loaded && <div className={styles.skeleton} />}
 			<div className={styles.gradient} />
-			<Carousel
-				autoPlay
-				infiniteLoop
-				swipeable
-				showStatus={false}
-				showIndicators={false}
-				showThumbs={false}
-				interval={5000}
-			>
-				<div>
-					<img
-						src="https://links.papareact.com/gi1"
-						alt="audible original"
-						onLoad={() => !imageLoaded && setImageLoaded(true)}
-					/>
-				</div>
-				<div>
-					<img
-						loading="lazy"
-						src="https://links.papareact.com/6ff"
-						alt="prime video"
-					/>
-				</div>
-				<div>
-					<img
-						loading="lazy"
-						src="https://links.papareact.com/7ma"
-						alt="amazon music"
-					/>
-				</div>
-			</Carousel>
+			<div style={{ display: loaded ? "block" : "none" }}>
+				<img
+					src="https://links.papareact.com/gi1"
+					alt="audible original"
+					className={styles.image}
+					onLoad={() => !imageLoaded && setImageLoaded(true)}
+				/>
+			</div>
 		</div>
 	);
 };
