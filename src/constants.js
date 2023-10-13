fd8fc4feb340e8362ee0d4730683e855aa998610
TropicolX <jacobsbusayo@gmail.com>
2023-10-13 14:38:45 +0100

    cloudinary for images

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
-export const bannerUrl = "https://links.papareact.com/gi1";
-export const logoUrl = "https://links.papareact.com/f90";
+export const bannerUrl =
+	"https://res.cloudinary.com/tropicolx/image/upload/v1697204098/e_commerce_app/audible_qow5oa.jpg";
+export const logoUrl =
+	"https://res.cloudinary.com/tropicolx/image/upload/v1697204091/e_commerce_app/amazon-logo_px2uok.png";
 export const amazonChoiceUrl =
-	"https://m.media-amazon.com/images/G/01/amazonsChoice/acBadge-p13n.svg";
+	"https://res.cloudinary.com/tropicolx/image/upload/v1697204289/e_commerce_app/amazon-choice-badge_xv9uux.svg";
