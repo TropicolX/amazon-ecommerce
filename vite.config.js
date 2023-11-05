d380bb50c6bb1ac93622fb813942a840c8ab7bcb
TropicolX <jacobsbusayo@gmail.com>
2023-11-05 15:47:30 +0100

    testing no compression

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteCompression from "vite-plugin-compression";
+// import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		viteCompression({
-			verbose: true,
-			filter: undefined,
-		}),
+		// viteCompression({
+		// 	filter: undefined,
+		// }),
 	],
 });
